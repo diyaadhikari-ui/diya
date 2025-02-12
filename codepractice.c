90e04b85c60bff75d7f9e7325819d34ed407788d
unknown <diyaadhikari027@gmail.com>
2025-02-12 13:50:26 +0700

    first commit

---
+++ codepractice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    char citizen;
+
+    // Asking user for age and citizenship status
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Are you a citizen? (yes/no): ");
+    scanf(" %c",&citizen): // space before %c to consume
+
